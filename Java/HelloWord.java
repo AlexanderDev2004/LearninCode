01ce6f001fa04a097e446847d5c7c1f3587a14bd
AlexanderDev2004 <alexgaming2120@gmail.com>
2024-05-09 23:29:41 +0700

    Day1_HelloWorld.Java

---
+++ Java/HelloWord.java
@@ -0,0 +1,7 @@
+package Java;
+public class HelloWord {  // jangan pernah ada spasi contoh Hello word Harus HelloWord atau kalian bisa Hello_word
+    public static void main(String[] args) { 
+        System.out.println("Hello word");
+    }
+    
+}
